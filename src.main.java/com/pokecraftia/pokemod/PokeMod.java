61d5f15d3bd54a39de03c7c730502ca203e60af3
vamp <vampireofh8@gmail.com>
2019-04-14 04:27:16 -0500

    added postInt. was missing from examplemod events O_o

--- src.main.java/com/pokecraftia/pokemod/PokeMod.java
+++ src.main.java/com/pokecraftia/pokemod/PokeMod.java
@@ -1,9 +1,9 @@
 package com.pokecraftia.pokemod;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -18,15 +18,17 @@ public class PokeMod
     private static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
     }
-
+ 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    public void init(FMLInitializationEvent event) {
+        logger.info(PokeMod.NAME + " has been loaded WAH!");
     }
+ 
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+ 
+}
 }
